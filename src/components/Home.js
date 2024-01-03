4fe836769d0f6aaf01ac323407d441b227a35830
Lt-Leinad <daniel.feddy0123@gmail.com>
2024-01-03 16:19:22 +0200

    first commit, sanity and sass set up in react

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="home-body">
+      <div className="section1">
+        <h1>Hello</h1>
+      </div>
+      <div className="section2">
+        <h1>World</h1>
+      </div>
+    </div>
+  );
+}
