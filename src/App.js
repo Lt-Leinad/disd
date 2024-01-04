8af71b582bd0f4b3f677ba534938b50b8b28bdd4
Lt-Leinad <daniel.feddy0123@gmail.com>
2024-01-04 15:13:25 +0200

    mobile version ongoing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Services from "./components/Services";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
+      <About />
+      <Services />
     </div>
   );
 }
---
+++ src/components/Services.js
@@ -0,0 +1,10 @@
+import "../App.scss";
+
+export default function Services() {
+  return (
+    <div className="services-body">
+      <h2>OUR SERVICES</h2>
+      <h3>Building software to help your business grow</h3>
+    </div>
+  );
+}
